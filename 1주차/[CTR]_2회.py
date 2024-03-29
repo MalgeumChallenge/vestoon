2906f6381cbb6a4e7c895487a39c36e37be75ee7
bestoon <148741679+vestoon@users.noreply.github.com>
2024-03-29 16:53:48 +0900

    김도윤_20240329_Upscaling(구현)

--- 1주차/[CTR]_2회.py
+++ 1주차/[CTR]_2회.py
@@ -6,20 +6,20 @@ for tc in range(1, t+1):
     n = int(input())
     x = "##"
     y = ".."
-    line1 = ''
-    line2 = ''
+    line1 = '' # #먼저 시작하는 줄
+    line2 = '' # .먼저 시작하는 줄
     for i in range(n//2):
         line1 += x+y
         line2 += y+x
-    if n%2:
+    if n%2: # 홀수인 경우 하나 추가
         line1 += x
         line2 += y
 
-    for j in range(n//2):
+    for j in range(n//2): # 행 길이만큼 반복
         print(line1)
         print(line1)
         print(line2)
         print(line2)
-    if n%2:
+    if n%2: # 홀수인 경우 하나 추가
         print(line1)
         print(line1)
