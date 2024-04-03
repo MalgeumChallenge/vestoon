ecc11dadce6bf28a3c44b4f462bfa868de6acd8d
bestoon <148741679+vestoon@users.noreply.github.com>
2024-04-04 00:41:31 +0900

    Add files via upload

---
+++ 2주차/2075.py
@@ -0,0 +1,13 @@
+import sys
+import heapq
+input = sys.stdin.readline
+
+N = int(input())
+arr = list(map(int, input().split()))
+for _ in range(N-1):
+    arr += list(map(int, input().split()))
+    arr.sort()
+    arr = arr[N:]
+
+print(arr[0])
+# print(arr)
