4a8d815baa564d3a38fe630f71a3847da237700b
bestoon <148741679+vestoon@users.noreply.github.com>
2024-04-04 14:56:38 +0900

    김도윤_20240404_21599(그리디,정렬)

--- 2주차/[BOJ]_5회.py
+++ 2주차/[BOJ]_5회.py
@@ -3,6 +3,7 @@ import sys
 import heapq
 input = sys.stdin.readline
 
+# 그냥 내림차순으로 중복이 최대가 되도록 정렬한 후 선형으로 탐색하면서 강화되는 아이템의 수를 갱신하면 되는 문제
 N = int(input())
 items = list(map(int, input().split()))
 items.sort(reverse=True)
