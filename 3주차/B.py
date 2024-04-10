ede3b90e70dc2a56c1ce5dbef428edfe6158b1d7
bestoon <148741679+vestoon@users.noreply.github.com>
2024-04-10 15:47:26 +0900

    Add files via upload

---
+++ 3주차/B.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+t = int(input())
+for tc in range(1, t+1):
+    n, c, d = map(int, input().split())
+    elements = list(map(int, input().split()))
+    elements.sort()
+    a = elements[0]
+    correct = []
+
+    for i in range(n):
+        for j in range(n):
+            correct.append(a + i*c + j*d)
+    correct.sort()
+
+    if correct == elements:
+        print("YES")
+    else:
+        print("NO")
