3385dd9e5ee732a48fa8c5f827cc89122bbfe67d
bestoon <148741679+vestoon@users.noreply.github.com>
2024-05-03 01:48:28 +0900

    Add files via upload

---
+++ 17103.py
@@ -0,0 +1,28 @@
+import sys
+import math
+input = sys.stdin.readline
+
+isPrime = [True for x in range(1000001)]
+isPrime[0] = False
+for i in range(2, 1001):
+    if not isPrime[i]:
+        continue
+
+    for j in range(i+i, 1000001, i):
+        isPrime[j] = False
+
+# for i in range(2, 101):
+#     if isPrime[i]:
+#         print(i, end=' ')
+
+
+T = int(input())
+for tc in range(1, T+1):
+    N = int(input())
+    cnt = 0
+    for i in range(2, N//2 + 1):
+        # print('i', i)
+        if isPrime[i] and isPrime[N-i]:
+            cnt += 1
+    
+    print(cnt)
