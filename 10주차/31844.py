9677038662b722dc02b4eedd313b782d2fdd4876
bestoon <148741679+vestoon@users.noreply.github.com>
2024-06-02 17:11:11 +0900

    Add files via upload

---
+++ 10주차/31844.py
@@ -0,0 +1,23 @@
+import sys
+input = sys.stdin.readline
+
+# 1.로봇을 박스에 인접한 곳으로 이동
+# 2. 밀어서 옮기기
+
+info = input().rstrip()
+
+rob , box, flag = -1, -1, -1
+for i in range(10):
+    if info[i] == '#':
+        box = i
+    elif info[i] == '@':
+        rob = i
+    elif info[i] == '!':
+        flag = i
+
+if (flag < box and rob < box) or ( box < flag and box < rob):
+    print(-1)
+    exit()
+
+# print(rob, box, flag)
+print(abs(rob-box)-1 + abs(box-flag))
